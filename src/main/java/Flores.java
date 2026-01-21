bf49c910626cd17c06cf73027510e85e8d2b5de0
AdibShifas <shifasadib@gmail.com>
2026-01-22 02:30:16 +0800

    Level-3: Mark as Done

--- src/main/java/Flores.java
+++ src/main/java/Flores.java
@@ -10,23 +10,45 @@ public class Flores {
         System.out.println(horizontalLine);
         Scanner sc = new Scanner(System.in);
         // arraylist to store each input
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> items = new ArrayList<>();
         while (true) {
             String input = sc.nextLine();
             System.out.println(horizontalLine);
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < items.size(); i++) {
                     System.out.println((i + 1) + ". " + items.get(i));
                 }
                 System.out.println(horizontalLine);
                 continue;
             }
+            if (input.startsWith("mark ")) {
+                // used google gemini for this logic
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                items.get(index).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + items.get(index));
+                System.out.println(horizontalLine);
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                // used google gemini for this logic
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                items.get(index).markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + items.get(index));
+                System.out.println(horizontalLine);
+                continue;
+            }
+
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(horizontalLine);
                 break;
             }
-            items.add(input);
+
+            items.add(new Task(input));
             System.out.println("added: " + input);
             System.out.println(horizontalLine);
 
