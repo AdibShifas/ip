040bd6021a3ff8c45d379c0f1638b625b48d9812
AdibShifas <shifasadib@gmail.com>
2026-01-21 18:37:56 +0800

    Level-2: Add, List

--- src/main/java/Flores.java
+++ src/main/java/Flores.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Flores {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
 
-        //greet user
+        // greet user
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Flores\nWhat can I do for you?");
         System.out.println(horizontalLine);
         Scanner sc = new Scanner(System.in);
+        // arraylist to store each input
+        ArrayList<String> items = new ArrayList<>();
         while (true) {
             String input = sc.nextLine();
             System.out.println(horizontalLine);
-
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i + 1) + ". " + items.get(i));
+                }
+                System.out.println(horizontalLine);
+                continue;
+            }
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(horizontalLine);
                 break;
             }
-            System.out.println(input);
+            items.add(input);
+            System.out.println("added: " + input);
             System.out.println(horizontalLine);
 
         }
