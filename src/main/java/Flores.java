7d324ae40a82273ed8be936634ecb386b4c29c0b
AdibShifas <shifasadib@gmail.com>
2026-01-21 18:23:35 +0800

    Level-1: Echo

--- src/main/java/Flores.java
+++ src/main/java/Flores.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class Flores {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        String horizontalLine = "____________________________________________________________";
+
+        //greet user
+        System.out.println(horizontalLine);
         System.out.println("Hello! I'm Flores\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(horizontalLine);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(horizontalLine);
+
+        }
+        sc.close();
     }
 }
