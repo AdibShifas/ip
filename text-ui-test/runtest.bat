fca0d65dfd467ad98bf6e778803218f9600b02db
Adib Shifas <shifasadib@gmail.com>
2026-02-10 15:58:04 +0800

    Fix Bug in CI

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,8 +6,12 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete data from previous run to ensure clean state
+if exist data\flores.txt del data\flores.txt
+
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM compile the code into the bin folder
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\flores\Flores.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -15,7 +19,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin flores.Flores < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
